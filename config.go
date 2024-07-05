13fb6d43900866bcb63a0c9bf1dde542cea88a75
Andri Winata <3tagger95@gmail.com>
2024-07-05 13:23:31 +0700

    feat: adding load config and object

---
+++ config.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Server ServerConfig
+}
+
+type ServerConfig struct {
+	Host string
+	Post string
+}
+
+func LoadConfig() Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
+	return Config{
+		Server: loadServerConfig(),
+	}
+
+}
+
+func loadServerConfig() ServerConfig {
+	return ServerConfig{
+		Host: os.Getenv("SERVER_HOST"),
+		Post: os.Getenv("SERVER_PORT"),
+	}
+}
